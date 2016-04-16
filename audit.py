c2fd7692c47f15167fd2c835005caa3de1133a17
Luke Hinds <lukehinds@gmail.com>
2016-04-16 18:23:01 +0100

    main file

---
+++ audit.py
@@ -0,0 +1,66 @@
+# !/usr/bin/python
+
+import os
+import subprocess
+
+dirlist = os.walk('.').next()[1]
+projects = ['Compliance','apex','armband','availability','bottlenecks','compass4nfv','conductor','copper','cperf','doctor','domino','dovetail','dpacc','enfv','escalator','fastpathmetrics','fds','fuel','functest','genesis','genesisreq','inspector','ipv6','joid','kvmfornfv','lsoapi','models','moon','movie','multisite','netready','octopus','onosfw','oscar','ovno','ovsnfv','parser','pharos','pinpoint','policytest','prediction','promise','qtip','releng','rs','sandbox','sdnvpn','securedlab','sfc','storperf','vnf_forwarding_graph','vswitchperf','yardstick']
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def update():
+    for project in  dirlist:
+        print 'Pulling updates from {0}\n'.format(project)
+        os.chdir(os.path.join(script_dir.rstrip(), project))
+        subprocess.call(["git", "pull"])
+        print '\n'
+
+
+def clone():
+    for project in projects:
+        if os.path.isdir(project):
+            print 'A clone of {0} already exists'.format(project)
+        else:
+            proj = "git clone https://gerrit.opnfv.org/gerrit/{0}".format(project)
+            subprocess.call([(proj)], shell=True)
+            
+
+def audit(project):
+    py = 0
+    sh = 0
+    java = 0
+    c = 0
+    for dirname, dirnames, filenames in os.walk(project):
+        for filename in filenames:
+            if filename.endswith('.py'):
+                py = py +1
+            elif filename.endswith('.sh'):
+                sh = sh +1
+            elif filename.endswith('.java'):
+                java = java +1
+            elif filename.endswith('.c'):
+                c = c +1
+    print '{0} python files'.format(py)
+    print '{0} shellscript files'.format(sh)
+    print '{0} java files'.format(java)
+    print '{0} c files\n'.format(c)
+
+
+def main():
+    for project in  dirlist:
+        print '{0} contains:\n'.format(project)
+        audit(project)
+
+if __name__ == "__main__":
+    choice = None
+    print "(c)lone or (u)pdate or (a)udit\n"
+    val = raw_input("Enter Option: ").lower()
+    if val == 'c':
+        clone()
+    elif val == 'u':
+        update()
+    elif val == 'a':
+        main()
+    else:
+        print "Not an option!"
