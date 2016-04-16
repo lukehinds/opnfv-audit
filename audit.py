57633ee28fd9a5603cde3ff46418f962eee8876f
lukehinds <lukehinds@gmail.com>
2016-04-16 21:18:06 +0100

    Added python import module list builder

--- audit.py
+++ audit.py
@@ -11,11 +11,11 @@
 # 0.1: This script provides an audit of all files in opnfv repos
 
 import os
+import re
 import subprocess
 
 dirlist = os.walk('.').next()[1]
 projects = ['Compliance','apex','armband','availability','bottlenecks','compass4nfv','conductor','copper','cperf','doctor','domino','dovetail','dpacc','enfv','escalator','fastpathmetrics','fds','fuel','functest','genesis','genesisreq','inspector','ipv6','joid','kvmfornfv','lsoapi','models','moon','movie','multisite','netready','octopus','onosfw','oscar','ovno','ovsnfv','parser','pharos','pinpoint','policytest','prediction','promise','qtip','releng','rs','sandbox','sdnvpn','securedlab','sfc','storperf','vnf_forwarding_graph','vswitchperf','yardstick']
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -34,9 +34,15 @@ def clone():
         else:
             proj = "git clone https://gerrit.opnfv.org/gerrit/{0}".format(project)
             subprocess.call([(proj)], shell=True)
-            
+
+def nextwork(target, source):
+    for i, w in enumerate(source):
+        if w == target:
+            return source[i+1]
 
 def audit(project):
+    pyimps = []
+    javaimp = []
     py = 0
     sh = 0
     java = 0
@@ -45,21 +51,30 @@ def audit(project):
         for filename in filenames:
             if filename.endswith('.py'):
                 py = py +1
+                with open(os.path.join(dirname.rstrip(), filename)) as f:
+                    for line in f.readlines():
+                        match = re.search(r'import (\w+)', line)
+                        if match:
+                            pyimps.append(match.group(1))
             elif filename.endswith('.sh'):
                 sh = sh +1
             elif filename.endswith('.java'):
                 java = java +1
             elif filename.endswith('.c'):
                 c = c +1
-    print '{0} python files'.format(py)
+    print '{0} python files\n'.format(py)
+    print 'Python modules Imported:\n'
+    print pyimps
+    print '\n'
     print '{0} shellscript files'.format(sh)
+    print '\n'
     print '{0} java files'.format(java)
     print '{0} c files\n'.format(c)
 
 
 def main():
     for project in  dirlist:
-        print '{0} contains:\n'.format(project)
+        print 'Project {0} contains:\n'.format(project)
         audit(project)
 
 if __name__ == "__main__":
