eed51a53b68abdb653bae1d98f6e065f0267003a
lukehinds <lukehinds@gmail.com>
2016-04-17 09:03:58 +0100

    Formatting clean up, and guide comments added

--- audit.py
+++ audit.py
@@ -32,16 +32,14 @@ UNKNOWN = START+BOLD+YELLOW
 TITLE = START+BOLD+PURPLE
 MESSAGE = START+BOLD+BLUE
 
-dirlist = os.walk('.').next()[1]
 projects = ['Compliance','apex','armband','availability','bottlenecks','compass4nfv','conductor','copper','cperf','doctor','domino','dovetail','dpacc','enfv','escalator','fastpathmetrics','fds','fuel','functest','genesis','genesisreq','inspector','ipv6','joid','kvmfornfv','lsoapi','models','moon','movie','multisite','netready','octopus','onosfw','oscar','ovno','ovsnfv','parser','pharos','pinpoint','policytest','prediction','promise','qtip','releng','rs','sandbox','sdnvpn','securedlab','sfc','storperf','vnf_forwarding_graph','vswitchperf','yardstick']
 script_dir = os.path.dirname(os.path.abspath(__file__))
-
+dirlist = os.walk('.').next()[1]
 
 def update():
     for project in  dirlist:
         sys.stdout.write("%sPulling updates from %s%s" % (MESSAGE, project, RESET))
         sys.stdout.write("\n\n")
-        #print 'Pulling updates from {0}\n'.format(project)
         os.chdir(os.path.join(script_dir.rstrip(), project))
         subprocess.call(["git", "pull"])
         sys.stdout.write("\n")
@@ -59,24 +57,36 @@ def clone():
 
 
 def audit(project):
+    # List for python imports
     pyimps = []
+    # List for Java imports
     javaimp = []
+    # List for C Includes
     cinclude = []
+    # Counter for the amount of Python Files
     py = 0
+    # Counter for the amount of shell scripts
     sh = 0
+    # Counter for the amount of Java files
     java = 0
+    # Counter for the amount of C source files
     c = 0
     for dirname, dirnames, filenames in os.walk(project):
         for filename in filenames:
-            # check if python files
+            # check if python file
             if filename.endswith('.py'):
+                # Lets count it
                 py = py +1
+                # Open the file to gather modules into pyimp list
                 with open(os.path.join(dirname.rstrip(), filename)) as f:
                     for line in f.readlines():
+                        # Regex match for import (e.g. import subprocess)
                         match = re.search(r'import (\w+)', line)
                         if match:
+                            # Append each module to the pyimps list
                             pyimps.append(match.group(1))
             elif filename.endswith('.sh'):
+                # Count shell scripts
                 sh = sh +1
             elif filename.endswith('.java'):
                 java = java +1
@@ -87,6 +97,9 @@ def audit(project):
                             try:
                                 javaimp.append(line[1])
                             except IndexError:
+                                # Might need to improve on pass, its mainly
+                                # there to capture out stuff that has proved
+                                # useless
                                 pass
             elif filename.endswith('.c'):
                 c = c +1
@@ -101,9 +114,12 @@ def audit(project):
     if py > 1:
         sys.stdout.write("%s%s python files found.%s" % (MESSAGE,py, RESET))
         sys.stdout.write("\n\n")
-        #print '{0} python files found.\n'.format(py)
+        # Report the amount of Python Scrips found using the 'py counter'
+        print '{0} python files found.\n'.format(py)
+        # Print out the import modules found, in a nicer format then a long
+        # python list
         sys.stdout.write("%sPython modules Imported:%s" % (MESSAGE, RESET))
-        sys.stdout.write("\n\n")
+        sys.stdout.write("\n")
         # Remove duplicates
         pyimps = list(set(pyimps))
         print('\n'.join('{}: {}'.format(*k) for k in enumerate(pyimps)))
@@ -114,7 +130,7 @@ def audit(project):
         sys.stdout.write("%s%s python files found.%s" % (MESSAGE,java, RESET))
         sys.stdout.write("\n\n")
         sys.stdout.write("%sJava modules Imported:%s" % (MESSAGE, RESET))
-        sys.stdout.write("\n\n")
+        sys.stdout.write("\n")
         # Remove duplicates
         javaimp = list(set(javaimp))
         print('\n'.join('{}: {}'.format(*k) for k in enumerate(javaimp)))
@@ -123,7 +139,7 @@ def audit(project):
         sys.stdout.write("\n\n")
         # Remove duplicates
         sys.stdout.write("%sC libraries Imported:%s" % (MESSAGE, RESET))
-        sys.stdout.write("\n\n")
+        sys.stdout.write("\n")
         cinclude = list(set(cinclude))
         print('\n'.join('{}: {}'.format(*k) for k in enumerate(cinclude)))
     if py < 1 and sh < 1 and java < 1 and c < 1:
@@ -137,7 +153,8 @@ def main():
 
 
 if __name__ == "__main__":
-    choice = None
+    # This needs work, would be better if it returned to prompt after each
+    # action
     print "(c)lone or (u)pdate or (a)udit\n"
     val = raw_input("Enter Option: ").lower()
     print '\r'
